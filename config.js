7701dd4f9b2ec4742619043f9faf2009db7e60c3
vnaik <vnaik@734X64A.bsihq.com>
2016-09-07 12:29:56 -0400

    Added more..

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8888,
+	'database':'mongodb://winitn:winitn@ds153715.mlab.com:53715/feelthegas?authSource=feelthegas',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
