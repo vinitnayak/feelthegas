98231ceab0a9162134e32d79d5b4c0419471af04
vnaik <vnaik@734X64A.bsihq.com>
2016-09-07 17:16:53 -0400

    Add some more

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,36 @@
-/**
- * http://usejsdoc.org/
- */
+angular.module('routerApp', ['routerRoutes','ngAnimate'])
+
+// create the controller and inject Angular's 
+// this will be the controller for the ENTIRE site
+.controller('mainController', function() {
+
+	var vm = this;
+
+    	// create a bigMessage variable to display in our view
+    	vm.bigMessage = 'A smooth sea never made a skilled sailor.';
+
+})
+
+// home page specific controller
+.controller('homeController', function() {
+
+	var vm = this;
+
+	vm.message = 'This is the home page!111';
+})
+
+// about page controller
+.controller('aboutController', function() {
+
+	var vm = this;
+
+    	vm.message = 'Look! I am an about page.';
+})
+
+// contact page controller
+.controller('contactController', function() {
+
+	var vm = this;
+
+    	vm.message = 'Contact us! JK. This is just a demo.';
+});
--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,3 +1,30 @@
-/**
- * http://usejsdoc.org/
- */
+// inject ngRoute for all our routing needs
+angular.module('routerRoutes', ['ngRoute'])
+
+// configure our routes
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'app/views/pages/home.html',
+            controller  : 'homeController',
+            controllerAs: 'home'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'app/views/pages/about.html',
+            controller  : 'aboutController',
+            controllerAs: 'about'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'app/views/pages/contact.html',
+            controller  : 'contactController',
+            controllerAs: 'contact'
+        });
+
+    $locationProvider.html5Mode(true);
+});
